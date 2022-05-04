672ec649f63d213c8a56653f4d16dae1ad2ec8bd
z-ira <104900889+z-ira@users.noreply.github.com>
2022-05-04 16:49:57 +0530

    Add files via upload

---
+++ T1 area of circle.py
@@ -0,0 +1,7 @@
+#area of circle
+import math
+radius=float(input("Enter radius: "));
+print(radius);
+a=math.pi*radius*radius
+print("Area of circle is: ");
+print(a);
---
+++ T2 extenion.py
@@ -0,0 +1,12 @@
+#Program to accept file name and print the extention
+
+Fname=input("Enter file name with extention: ");
+Fext=Fname.split(".")
+
+if Fext[-1]=="py":
+    Fext[-1]="python"
+elif Fext[-1]=="cpp":
+    Fext[-1]="C++"
+elif Fext[-1]=="c":
+    Fext[-1]="C"
+print(" Extention of file is: "+ repr(Fext[-1]));
